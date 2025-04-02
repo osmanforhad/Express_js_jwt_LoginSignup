e8ea3e6810784bdac490ae92ebe55bbe79cf74fb
osmanforhad <osmanforhad93@gmail.com>
2025-04-02 19:15:37 +0600

    setup route for admin account

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import SignupController from "../controllers/UserSignupController.js";
+import createAdminAccount from "../AdminScripts/Admin.js";
 
 const route = express.Router();
 
 route.post("/register", SignupController);
+route.post("/admin", createAdminAccount);
 
 export default route;
